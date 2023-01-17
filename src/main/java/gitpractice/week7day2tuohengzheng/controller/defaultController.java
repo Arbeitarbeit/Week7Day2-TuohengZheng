b3c2796a2cfadc86c7703e8eee3860a19af28fa7
Tuoheng Zheng <69173664+Arbeitarbeit@users.noreply.github.com>
2023-01-17 16:55:04 -0600

    Basic setup for application created controller edited application.properties

---
+++ src/main/java/gitpractice/week7day2tuohengzheng/controller/defaultController.java
@@ -0,0 +1,10 @@
+package gitpractice.week7day2tuohengzheng.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class defaultController {
+
+}
